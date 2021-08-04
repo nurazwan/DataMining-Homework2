9610cc201bb75d652e224696f41cbb71b572122b
nurazwan <64972402+nurazwan@users.noreply.github.com>
2021-08-04 22:34:12 +0800

    first commit

---
+++ RandomForest_pca.py
@@ -0,0 +1,16 @@
+from data_prep import get_data_reduced
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from joblib import dump
+
+X_train, X_test, y_train, y_test = get_data_reduced()
+
+F_params={'n_estimators':[100,200,400,800,1600],'criterion':['gini', 'entropy'], 
+'max_depth':[10,20,30,40,50,60,70,80,90,100,None], 'min_samples_split':[2,3,4,5,6], 'min_samples_leaf':[2,4,16],'bootstrap':[True,False],
+'oob_score':[True,False]}
+
+Model=GridSearchCV(RandomForestClassifier(),param_grid=F_params,cv=10,verbose=5, n_jobs=-1,scoring='recall')
+RF_model=Model.fit(X_train,y_train)
+
+dump(RF_model,'RandomForest_pca.joblib')
